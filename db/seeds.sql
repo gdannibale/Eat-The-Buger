7ebd14201df3800bc0396c9f0c65a0f2c391ab67
Gina Dannibale <ginadannibale@gmail.com>
2020-11-11 14:41:05 -0600

    Added folders

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Classic Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom and Swiss Burger');
